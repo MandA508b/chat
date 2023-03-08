13c91a66189a559d5414c2bf470849d6cf06a14d
Andriy Derkach <andriyderk@gmail.com>
2023-03-09 00:53:52 +0200

    fix/res chat

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const cors = require('cors');
+const Advertisement = require('./models/advertisement.model')
 
 const { addUser, getUser, getUsersInRoom } = require('./controllers/chatUser.controller');
 const { addMessage, getAllMessageByChatName } = require('./controllers/chatData.controller');
@@ -50,6 +51,16 @@ io.on('connect', (socket) => {
 
   socket.on('sendMessage', async (message, callback) => {
     try{
+      const advertisementId = user.room.slice(0, 24)
+      const advertisement = await Advertisement.findById(advertisementId)
+
+      if(advertisement.statusStage === "reserved"){
+        return  io.to(user.room).emit('message', { user: "LigaBot" , text: "Наразі ви не можете надсилати повідомлення до цього чату, зарезервований інший чат до цього оголошення!" , date: chatData.date});
+      }else
+        if(advertisement.statusStage === "fixed"){
+         return io.to(user.room).emit('message', { user: "LigaBot" , text: "Наразі ви не можете надсилати повідомлення до цього чату, зафіксований інший чат до цього оголошення!" , date: chatData.date});
+        }
+
       const user = await getUser(socket.id);
       const chatData = await addMessage(user.room, user.name, message)
       io.to(user.room).emit('message', { user: user.name , text: message , date: chatData.date});
---
+++ server/models/advertisement.model.js
@@ -0,0 +1,65 @@
+const db= require('../db/db')
+
+const schema = new db.Schema({
+    status: {
+        type: Boolean,
+        default: true
+    },
+    number: {
+        type: Number,
+        required: true
+    },
+    userId:{
+        type: db.Schema.Types.ObjectId,
+        required: true
+    },
+    leagueId:{
+        type: db.Schema.Types.ObjectId,
+        required:true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    cityId:{
+        type: db.Schema.Types.ObjectId,
+        required: true
+    },
+    total:{
+        type: Number,
+        required: true
+    },
+    part: {
+        type: Number,
+        default: 0
+    },
+    rate:{
+        type: Number,
+        required: true
+    },
+    deadline:{
+        type: String,
+        required: true
+    },
+    extraInfo: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    },
+    statusStage: {// open, reserved, fixed
+        type: String,
+        default: "open"
+    },
+    linkedChat: {
+        type: db.Schema.Types.ObjectId,
+        default: "000000000000000000000000"//24
+    },
+    channelMessageId: {
+        type: Number,
+        default: 0
+    }
+})
+
+module.exports = db.model('Advertisement', schema)
